2ae56d28b64e22e84817b320b4da7a564ef11fe4
yuzhilin <1009342713@qq.com>
2021-10-22 15:54:23 +0800

    commit

--- webrtc-draw-guess/server/index copy.js
+++ webrtc-draw-guess/server/index copy.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express()
 const server = require('http').Server(app);
 const Socket = require('socket.io')
-const io = require('socket.io')(server, {allowEIO3: true, cors: true });
+const io = require('socket.io')(server, { allowEIO3: true, cors: true });
 // 解决跨域问题
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-  app.use(express.static('../drawApp/public'));
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+app.use(express.static('../drawApp/public'));
 // 保存user
 const users = {}
 // 保存房间
@@ -19,11 +19,11 @@ const rooms = {}
 const sockets = {}
 
 // 题目 
-const question = ['花','对牛弹琴', '冰糖葫芦', '刻舟求剑', '口红', '七上八下', '放风筝', '台灯',"理发师", "雷人", "不入虎穴焉得虎子", "仙人掌", "耳机", "打火机", "汉堡", "画饼充饥", "虎头蛇尾", "泪流满面", "捧腹大笑", "画蛇添足", "一手遮天", "掩耳盗铃", "布娃娃", "娃哈哈", "CD", "落地灯", "内裤", "烟斗", "鹦鹉", "钻戒", "网址", "牛肉面"]
+const question = ['花', '对牛弹琴', '冰糖葫芦', '刻舟求剑', '口红', '七上八下', '放风筝', '台灯', "理发师", "雷人", "不入虎穴焉得虎子", "仙人掌", "耳机", "打火机", "汉堡", "画饼充饥", "虎头蛇尾", "泪流满面", "捧腹大笑", "画蛇添足", "一手遮天", "掩耳盗铃", "布娃娃", "娃哈哈", "CD", "落地灯", "内裤", "烟斗", "鹦鹉", "钻戒", "网址", "牛肉面"]
 
 // 用户类
 class User {
-  constructor(name, socre = 0, state=false, isoption=false) {
+  constructor(name, socre = 0, state = false, isoption = false) {
     this.name = name
     this.socre = socre
     this.state = state
@@ -61,7 +61,7 @@ class Room {
 function getRoomList(rooms) {
   let r = Object.keys(rooms)
   let room = r.map(room => {
-    return {room: room, length: rooms[room].users.length}
+    return { room: room, length: rooms[room].users.length }
   })
   return room
 }
@@ -76,8 +76,8 @@ function getRoomUsers(data) {
 io.on('connection', socket => {
   // 初始化
   socket.on('init', (data) => {
-      console.log("init",data);
-    socket.emit('init', {room: getRoomList(rooms), roomusers: getRoomUsers(data)})
+    console.log("init", data);
+    socket.emit('init', { room: getRoomList(rooms), roomusers: getRoomUsers(data) })
   })
   // 创建账户
   socket.on('createUser', data => {
@@ -87,38 +87,35 @@ io.on('connection', socket => {
   })
   // 创建房间
   socket.on('createRoom', data => {
-    console.log("createRoom",data);
-    socket.join(data.room, () => {
-      let room = new Room(data.room)
-      rooms[data.room] = room
-    }) 
+    console.log("createRoom", data);
+    socket.join(data.room)
+    let room = new Room(data.room)
+    rooms[data.room] = room
   })
   // 加入房间
   socket.on('joinRoom', data => {
     // socket api 加入房间 (房间名字， 回调)
-    socket.join(data.room, () => {
-      console.log("joinRoom",data);
-      let user = new User(data.user)
-      rooms[data.room].addUser(user)
-      socket.emit('broadcast', {msg: '你已经加入了房间'})
-      socket.to(data.room).emit('broadcast', {msg: `${data.user}已经加入了房间`})
-      io.in(data.room).emit('joinedRoom', {roomusers: getRoomUsers(data), joinUser: data.user})
-      io.emit('roomChange', {room: getRoomList(rooms)})
-    })
+    socket.join(data.room)
+    console.log("joinRoom", data);
+    let user = new User(data.user)
+    rooms[data.room].addUser(user)
+    socket.emit('broadcast', { msg: '你已经加入了房间' })
+    socket.to(data.room).emit('broadcast', { msg: `${data.user}已经加入了房间` })
+    io.in(data.room).emit('joinedRoom', { roomusers: getRoomUsers(data), joinUser: data.user })
+    io.emit('roomChange', { room: getRoomList(rooms) })
   })
   // 离开房间
   socket.on('leaveRoom', data => {
-      console.log("leaveRoom",data);
-    socket.leave(data.room, () => {
-      rooms[data.room].deleteUser(data.user)
-      if (!rooms[data.room].users.length) {
-        delete rooms[data.room]
-      }
-      socket.emit('broadcast', {msg: '你已经离开了房间'})
-      socket.to(data.room).emit('broadcast', {msg: `${data.user}已经离开了房间`})
-      io.emit('roomChange', {room: getRoomList(rooms)}) 
-      socket.to(data.room).emit('leaveRoom', {roomusers: getRoomUsers(data), leaveUser: data.user})
-    })
+    console.log("leaveRoom", data);
+    socket.leave(data.room)
+    rooms[data.room].deleteUser(data.user)
+    if (!rooms[data.room].users.length) {
+      delete rooms[data.room]
+    }
+    socket.emit('broadcast', { msg: '你已经离开了房间' })
+    socket.to(data.room).emit('broadcast', { msg: `${data.user}已经离开了房间` })
+    io.emit('roomChange', { room: getRoomList(rooms) })
+    socket.to(data.room).emit('leaveRoom', { roomusers: getRoomUsers(data), leaveUser: data.user })
   })
   // 准备
   socket.on('stateChange', (data) => {
@@ -135,21 +132,21 @@ io.on('connection', socket => {
       let room = data.room
       let index = rooms[room].index
       let users = rooms[room].users
-      let random =  Math.floor(Math.random() * 32)
+      let random = Math.floor(Math.random() * 32)
       users[index].isoption = true
       rooms[room].question = question[random]
-      sockets[users[index].name].to(room).emit('broadcast', {msg: `游戏已经开始，本轮你为猜题者`})
-      sockets[users[index].name].emit('broadcast', {msg: `游戏已经开始，本轮你为画手`})
-      io.to(room).emit('gameStart', {roomusers: getRoomUsers(data)})
-      sockets[users[index].name].to(room).emit('guessPage', {question: rooms[room].question.length})
-      sockets[users[index].name].emit('paintStart', {index: index, room: room, question: rooms[room].question})
+      sockets[users[index].name].to(room).emit('broadcast', { msg: `游戏已经开始，本轮你为猜题者` })
+      sockets[users[index].name].emit('broadcast', { msg: `游戏已经开始，本轮你为画手` })
+      io.to(room).emit('gameStart', { roomusers: getRoomUsers(data) })
+      sockets[users[index].name].to(room).emit('guessPage', { question: rooms[room].question.length })
+      sockets[users[index].name].emit('paintStart', { index: index, room: room, question: rooms[room].question })
     } else {
-      io.to(data.room).emit('stateChanged', {roomusers: getRoomUsers(data)})
+      io.to(data.room).emit('stateChanged', { roomusers: getRoomUsers(data) })
     }
   })
   socket.on('paintEnd', data => {
     socket.to(data.room).emit('guessStart')
-    socket.to(data.room).emit('broadcast', {msg: '已经绘画完成，请开始猜吧'})
+    socket.to(data.room).emit('broadcast', { msg: '已经绘画完成，请开始猜吧' })
   })
   socket.on('submitAnswer', data => {
     let users = rooms[data.room].users
@@ -167,15 +164,15 @@ io.on('connection', socket => {
     if (allOptioned) {
       if (rooms[data.room].index === 1) {
         console.log('gameEnd')
-        io.to(data.room).emit('gameEnd', {roomusers: getRoomUsers(data)})
-        io.to(data.room).emit('broadcast', {msg: `本次游戏已经结束，请观看积分`})
+        io.to(data.room).emit('gameEnd', { roomusers: getRoomUsers(data) })
+        io.to(data.room).emit('broadcast', { msg: `本次游戏已经结束，请观看积分` })
         rooms[data.room].init()
       } else {
         rooms[data.room].index++
-        io.to(data.room).emit('guessEnd', {roomusers: getRoomUsers(data)})
-        io.to(data.room).emit('broadcast', {msg: `第${rooms[data.room].index}轮已经结束，答案已经公布`})
+        io.to(data.room).emit('guessEnd', { roomusers: getRoomUsers(data) })
+        io.to(data.room).emit('broadcast', { msg: `第${rooms[data.room].index}轮已经结束，答案已经公布` })
       }
-      sockets[users[index].name].to(data.room).emit('showAnswer', {answer: rooms[data.room].question})
+      sockets[users[index].name].to(data.room).emit('showAnswer', { answer: rooms[data.room].question })
     }
   })
 
